5edbad46ab61b1e960f4c17a4af352980e7759bb
lizw <lzw1000000@163.com>
2020-07-13 20:43:21 +0800

    KafkaDataSource 增加api

--- clever-data-kafka/src/main/java/org/clever/data/kafka/KafkaDataSource.java
+++ clever-data-kafka/src/main/java/org/clever/data/kafka/KafkaDataSource.java
@@ -2,6 +2,7 @@ package org.clever.data.kafka;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.Metric;
 import org.apache.kafka.common.MetricName;
 import org.clever.data.common.AbstractDataSource;
@@ -12,6 +13,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.messaging.Message;
 import org.springframework.util.concurrent.ListenableFuture;
 
 import java.util.Map;
@@ -74,19 +76,126 @@ public class KafkaDataSource extends AbstractDataSource {
     // 消息操作 操作 TODO 封装API
     // --------------------------------------------------------------------------------------------
 
-    public Map<MetricName, ? extends Metric> getMetrics() {
-        return kafkaTemplate.metrics();
+    /**
+     * 发送数据到MQ
+     *
+     * @param topic     the topic
+     * @param partition the partition
+     * @param timestamp the timestamp of the record
+     * @param key       the key
+     * @param data      the data
+     */
+    public ListenableFuture<SendResult<Object, Object>> send(String topic, Integer partition, Long timestamp, Object key, Object data) {
+        return kafkaTemplate.send(topic, partition, timestamp, key, data);
     }
 
+    /**
+     * 发送数据到MQ
+     *
+     * @param topic     the topic
+     * @param partition the partition
+     * @param key       the key
+     * @param data      the data
+     */
+    public ListenableFuture<SendResult<Object, Object>> send(String topic, Integer partition, Object key, Object data) {
+        return kafkaTemplate.send(topic, partition, key, data);
+    }
+
+    /**
+     * 发送数据到MQ
+     *
+     * @param topic topic
+     * @param key   key
+     * @param data  data
+     */
     public ListenableFuture<SendResult<Object, Object>> send(String topic, Object key, Object data) {
         return kafkaTemplate.send(topic, key, data);
     }
 
-    public ListenableFuture<SendResult<Object, Object>> send(String topic, Object data) {
-        return kafkaTemplate.send(topic, data);
+    /**
+     * 发送数据到MQ
+     *
+     * @param message the Message
+     */
+    public ListenableFuture<SendResult<Object, Object>> send(Message<Object> message) {
+        return kafkaTemplate.send(message);
     }
 
+    /**
+     * 发送数据到MQ
+     *
+     * @param record the record
+     */
+    public ListenableFuture<SendResult<Object, Object>> send(ProducerRecord<Object, Object> record) {
+        return kafkaTemplate.send(record);
+    }
+
+    /**
+     * 发送数据到MQ
+     *
+     * @param partition the partition
+     * @param timestamp the timestamp of the record
+     * @param key       the key
+     * @param data      the data
+     */
+    public ListenableFuture<SendResult<Object, Object>> sendDefault(Integer partition, Long timestamp, Object key, Object data) {
+        return kafkaTemplate.sendDefault(partition, timestamp, key, data);
+    }
+
+    /**
+     * 发送数据到MQ
+     *
+     * @param partition the partition
+     * @param key       the key
+     * @param data      the data
+     */
+    public ListenableFuture<SendResult<Object, Object>> sendDefault(Integer partition, Object key, Object data) {
+        return kafkaTemplate.sendDefault(partition, key, data);
+    }
+
+    /**
+     * 发送数据到MQ
+     *
+     * @param key  the key
+     * @param data the data
+     */
+    public ListenableFuture<SendResult<Object, Object>> sendDefault(Object key, Object data) {
+        return kafkaTemplate.sendDefault(key, data);
+    }
+
+    /**
+     * 发送数据到MQ
+     *
+     * @param data the data
+     */
+    public ListenableFuture<SendResult<Object, Object>> sendDefault(Object data) {
+        return kafkaTemplate.sendDefault(data);
+    }
+
+    public void tmp() {
+//        kafkaTemplate.execute()
+//        kafkaTemplate.executeInTransaction();
+//        kafkaTemplate.getDefaultTopic();
+//        kafkaTemplate.inTransaction();
+//        kafkaTemplate.isTransactional()
+//        kafkaTemplate.partitionsFor()
+//        kafkaTemplate.sendOffsetsToTransaction();
+//        kafkaTemplate.setDefaultTopic();
+    }
+
+    // --------------------------------------------------------------------------------------------
+    // 其他 操作
+    // --------------------------------------------------------------------------------------------
+
+    /**
+     * 强制发送数据到服务端
+     */
     public void flush() {
         kafkaTemplate.flush();
     }
+
+    public Map<MetricName, ? extends Metric> getMetrics() {
+        return kafkaTemplate.metrics();
+    }
+
 }
