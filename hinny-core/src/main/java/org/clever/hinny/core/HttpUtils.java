3256ab5050f3429f464e6311c4f4af94ce777e87
lizw <1183409807@qq.com>
2020-08-10 22:08:04 +0800

    HttpUtils 3%

--- hinny-core/src/main/java/org/clever/hinny/core/HttpUtils.java
+++ hinny-core/src/main/java/org/clever/hinny/core/HttpUtils.java
@@ -5,4 +5,15 @@ package org.clever.hinny.core;
  * 创建时间：2020/07/28 22:32 <br/>
  */
 public class HttpUtils {
+    public static final HttpUtils Instance = new HttpUtils();
+
+    private final org.clever.common.utils.HttpUtils delegate;
+
+    private HttpUtils() {
+        delegate = org.clever.common.utils.HttpUtils.getInner();
+    }
+
+    public void test() {
+    }
+
 }
