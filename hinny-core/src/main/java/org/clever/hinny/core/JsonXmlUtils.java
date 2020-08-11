cbcd9186276fdd59891d67e7979eb35b4f9214fb
lizw <1183409807@qq.com>
2020-08-11 22:20:29 +0800

    HttpUtils 100% IDGenerateUtils 100%

--- hinny-core/src/main/java/org/clever/hinny/core/JsonXmlUtils.java
+++ hinny-core/src/main/java/org/clever/hinny/core/JsonXmlUtils.java
@@ -5,4 +5,12 @@ package org.clever.hinny.core;
  * 创建时间：2020/07/28 22:33 <br/>
  */
 public class JsonXmlUtils {
+    public static final JsonXmlUtils Instance = new JsonXmlUtils();
+
+    private JsonXmlUtils() {
+    }
+
+    public void test() {
+
+    }
 }
