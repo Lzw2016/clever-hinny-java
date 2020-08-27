e82c66070f3ff611d91c10ffc28f141baa06b8d3
lizw <lzw1000000@163.com>
2020-08-27 13:55:18 +0800

    暂存

--- hinny-mvc/src/main/java/org/clever/hinny/mvc/HttpRequestScriptHandler.java
+++ hinny-mvc/src/main/java/org/clever/hinny/mvc/HttpRequestScriptHandler.java
@@ -5,7 +5,7 @@ import com.google.common.cache.CacheBuilder;
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.method.HandlerMethod;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,7 +21,7 @@ import java.util.Set;
  * 创建时间：2020/08/24 21:34 <br/>
  */
 @Slf4j
-public class HttpRequestScriptHandler implements HandlerInterceptor {
+public class HttpRequestScriptHandler<T> implements HandlerInterceptor {
     /**
      * 没有js file映射时的数据前缀
      */
@@ -35,37 +35,39 @@ public class HttpRequestScriptHandler implements HandlerInterceptor {
      * 请求支持的后缀，建议使用特殊的后缀表示使用动态js代码处理请求
      */
     private static final Set<String> Support_Suffix = Collections.unmodifiableSet(new HashSet<>(Arrays.asList("", ".json", ".action")));
-    private static final String Debug_Use_Js_Handler = "debugUseJsHandler";
+//    private static final String Debug_Use_Js_Handler = "debugUseJsHandler";
 
-    /**
-     * 获取JS文件全名称 - 使用缓存
-     */
-    private String getJsHandlerFileFullNameUseCache(final HttpServletRequest request) {
-        final String path = request.getServletPath();
-        String jsHandlerFileFullName = Path_FileFullPath_Cache.getIfPresent(path);
-//        if (jsHandlerFileFullName != null && jsHandlerFileFullName.startsWith(No_FileFullPath_Mapping)) {
-//            final long time = NumberUtils.toLong(jsHandlerFileFullName.substring(No_FileFullPath_Mapping.length()), -1);
+//    /**
+//     * 获取JS文件全名称 - 使用缓存
+//     */
+//    private String getScriptFileFullPathUseCache(final HttpServletRequest request) {
+//
+//
+//        final String path = request.getServletPath();
+//        String scriptFileFullPath = Path_FileFullPath_Cache.getIfPresent(path);
+//        if (scriptFileFullPath != null && scriptFileFullPath.startsWith(No_FileFullPath_Mapping)) {
+//            final long time = NumberUtils.toLong(scriptFileFullPath.substring(No_FileFullPath_Mapping.length()), -1);
 //            if ((System.currentTimeMillis() - time) <= No_FileFullPath_Mapping_TimeOut) {
 //                return null;
 //            }
-//            jsHandlerFileFullName = null;
+//            scriptFileFullPath = null;
 //        }
-//        if (StringUtils.isNotBlank(jsHandlerFileFullName) && jsCodeFileExists(jsHandlerFileFullName)) {
-//            return jsHandlerFileFullName;
+//        if (StringUtils.isNotBlank(scriptFileFullPath) && jsCodeFileExists(scriptFileFullPath)) {
+//            return scriptFileFullPath;
 //        }
-//        jsHandlerFileFullName = getJsHandlerFileFullName(request);
-//        if (StringUtils.isNotBlank(jsHandlerFileFullName)) {
-//            Path_FileFullPath_Cache.put(path, jsHandlerFileFullName);
+//        scriptFileFullPath = getJsHandlerFileFullName(request);
+//        if (StringUtils.isNotBlank(scriptFileFullPath)) {
+//            Path_FileFullPath_Cache.put(path, scriptFileFullPath);
 //        } else {
 //            Path_FileFullPath_Cache.put(path, No_FileFullPath_Mapping + System.currentTimeMillis());
 //        }
-        return jsHandlerFileFullName;
-    }
+//        return scriptFileFullPath;
+//    }
 
-    /**
-     * 获取js模块对象处理请求
-     */
-    private ScriptObjectMirror getJsHandler(final String jsHandlerFileFullName, final HttpServletResponse response, final Object handler) {
+//    /**
+//     * 获取js模块对象处理请求
+//     */
+//    private ScriptObject getScriptHandler(final String scriptFileFullPath, final HttpServletResponse response, final Object handler) {
 //        final ScriptObjectMirror jsHandler = scriptModuleInstance.useJs(jsHandlerFileFullName);
 //        Object handlerObject = jsHandler.getMember(Handler_Method);
 //        if (!(handlerObject instanceof ScriptObjectMirror)) {
@@ -85,8 +87,7 @@ public class HttpRequestScriptHandler implements HandlerInterceptor {
 //            return null;
 //        }
 //        return jsHandler;
-        return null;
-    }
+//    }
 
     /**
      * 使用js代码处理请求
@@ -129,41 +130,58 @@ public class HttpRequestScriptHandler implements HandlerInterceptor {
         return startTime;
     }
 
+    @SuppressWarnings("NullableProblems")
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
-        if (handler instanceof HandlerMethod && request.getParameter(Debug_Use_Js_Handler) == null) {
-            return true;
-        }
-        // 获取JS文件全名称
-        final long startTime1 = System.currentTimeMillis();
-        final String jsHandlerFileFullName = getJsHandlerFileFullNameUseCache(request);
-        if (StringUtils.isBlank(jsHandlerFileFullName)) {
-            final long tmp = System.currentTimeMillis() - startTime1;
-            if (tmp > 0) {
-                log.debug("使用js代码处理请求 | 跳过js处理 | 总耗时 {}ms", tmp);
+        // 1.判断请求是否支持 Script 处理
+        // 2.获取处理请求的 Script 文件全路径
+        // 3.获取 Script 文件对应的 Script 对象和执行函数名
+        // 4.执行 Script 对象的函数
+
+        String requestUri = request.getRequestURI();
+        boolean supportUri = false;
+        for (String suffix : Support_Suffix) {
+            if (StringUtils.isBlank(suffix)) {
+                supportUri = true;
+                continue;
             }
-            return true;
+            if (requestUri.endsWith(suffix)) {
+                supportUri = true;
+                requestUri = requestUri.substring(0, requestUri.length() - suffix.length());
+                break;
+            }
         }
-        // 获取js模块对象处理请求
-        final long startTime2 = System.currentTimeMillis();
-        final ScriptObjectMirror jsHandler = getJsHandler(jsHandlerFileFullName, response, handler);
-        if (jsHandler == null) {
-            return true;
-        }
-        // 使用js代码处理请求
-        final long startTime3 = System.currentTimeMillis();
-        response.setHeader("use-http-request-js-handler", jsHandlerFileFullName);
-        final long startTime4 = doHandle(jsHandler, jsHandlerFileFullName, request, response);
-        // 请求处理完成 - 打印日志
-        long endTime = System.currentTimeMillis();
-        log.debug(
-                "使用js代码处理请求 | [{}] | [总]耗时 {}ms | [Js处理全过程]耗时 {}ms | [Js函数调用]耗时 {}ms | [返回值序列化]耗时 {}ms",
-                jsHandlerFileFullName,
-                endTime - startTime1,
-                endTime - startTime2,
-                endTime - startTime3,
-                endTime - startTime4
-        );
+//
+//        // 获取JS文件全名称
+//        final long startTime1 = System.currentTimeMillis();
+//        final String scriptFileFullPath = getScriptFileFullPathUseCache(request);
+//        if (StringUtils.isBlank(scriptFileFullPath)) {
+//            final long tmp = System.currentTimeMillis() - startTime1;
+//            if (tmp > 0) {
+//                log.debug("跳过script处理 | 总耗时 {}ms", tmp);
+//            }
+//            return true;
+//        }
+//        // 获取js模块对象处理请求
+//        final long startTime2 = System.currentTimeMillis();
+//        final ScriptObjectMirror jsHandler = getScriptHandler(scriptFileFullPath, response, handler);
+//        if (jsHandler == null) {
+//            return true;
+//        }
+//        // 使用js代码处理请求
+//        final long startTime3 = System.currentTimeMillis();
+//        response.setHeader("use-http-request-js-handler", jsHandlerFileFullName);
+//        final long startTime4 = doHandle(jsHandler, jsHandlerFileFullName, request, response);
+//        // 请求处理完成 - 打印日志
+//        long endTime = System.currentTimeMillis();
+//        log.debug(
+//                "使用js代码处理请求 | [{}] | [总]耗时 {}ms | [Js处理全过程]耗时 {}ms | [Js函数调用]耗时 {}ms | [返回值序列化]耗时 {}ms",
+//                jsHandlerFileFullName,
+//                endTime - startTime1,
+//                endTime - startTime2,
+//                endTime - startTime3,
+//                endTime - startTime4
+//        );
         return false;
     }
 
