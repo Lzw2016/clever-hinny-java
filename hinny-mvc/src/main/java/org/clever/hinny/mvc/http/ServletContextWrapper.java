ea1c44ccf2c00c3e93c5b0d7ce2a95724f691238
lizw <lzw1000000@163.com>
2020-08-31 17:06:22 +0800

    微调

--- hinny-mvc/src/main/java/org/clever/hinny/mvc/http/ServletContextWrapper.java
+++ hinny-mvc/src/main/java/org/clever/hinny/mvc/http/ServletContextWrapper.java
@@ -37,7 +37,7 @@ public class ServletContextWrapper {
     public ServletContext originalContext() {
         return delegate;
     }
-    
+
     /**
      * 返回一个于服务器上指定的URL进行通信的 ServletContext 对象
      */
@@ -234,58 +234,4 @@ public class ServletContextWrapper {
     public void setResponseCharacterEncoding(String encoding) {
         delegate.setResponseCharacterEncoding(encoding);
     }
-
-//    @Getter
-//    private final CurrentUserWrapper currentUserWrapper;
-//    @Getter
-//    private final HttpRequestWrapper requestWrapper;
-//    @Getter
-//    private final HttpSessionWrapper sessionWrapper;
-//    @Getter
-//    private final HttpResponseWrapper responseWrapper;
-//
-//    public ServletContextWrapper(HttpServletRequest request, HttpServletResponse response, JacksonMapper jacksonMapper) throws IOException {
-//        this.currentUserWrapper = new CurrentUserWrapper("lizw", "13260658831");
-//        this.requestWrapper = new HttpRequestWrapper(request);
-//        this.responseWrapper = new HttpResponseWrapper(response, jacksonMapper);
-//        HttpSession session = request.getSession(false);
-//        if (session == null) {
-//            this.sessionWrapper = null;
-//            this.servletContext = null;
-//        } else {
-//            this.sessionWrapper = new HttpSessionWrapper(session);
-//            this.servletContext = session.getServletContext();
-//        }
-//        if (servletContext != null) {
-//            init();
-//        }
-//    }
-//
-//    @SuppressWarnings("DuplicatedCode")
-//    private void init() {
-//        wrapper.put("currentUser", currentUserWrapper);
-//        wrapper.put("req", requestWrapper);
-//        wrapper.put("res", responseWrapper);
-//        wrapper.put("session", sessionWrapper);
-//        if (servletContext != null) {
-//            // --------------------------------------------------------------------- ServletContext attributes
-//            ScriptObjectMirror attributes = ScriptEngineUtils.newObject();
-//            Enumeration<String> attributeNames = servletContext.getAttributeNames();
-//            if (attributeNames != null) {
-//                while (attributeNames.hasMoreElements()) {
-//                    String attributeName = attributeNames.nextElement();
-//                    Object attributeValue = servletContext.getAttribute(attributeName);
-//                    attributes.put(attributeName, attributeValue);
-//                }
-//            }
-//            wrapper.put("attributes", attributes);
-//        }
-//    }
-//
-//    public void setAttribute(String name, Object value) {
-//        if (servletContext == null) {
-//            return;
-//        }
-//        servletContext.setAttribute(name, value);
-//    }
 }
