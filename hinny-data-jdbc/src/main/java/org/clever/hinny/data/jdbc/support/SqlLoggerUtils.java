3b86504ece05a94f0d8b74590988e5f8050abd9c
lizw <lzw1000000@163.com>
2020-09-02 14:11:20 +0800

    微调

--- hinny-data-jdbc/src/main/java/org/clever/hinny/data/jdbc/support/SqlLoggerUtils.java
+++ hinny-data-jdbc/src/main/java/org/clever/hinny/data/jdbc/support/SqlLoggerUtils.java
@@ -33,9 +33,7 @@ public class SqlLoggerUtils {
         if (!log.isDebugEnabled()) {
             return;
         }
-        if (sql != null && (sql.contains("\r\n") || sql.contains("\n"))) {
-            sql = sql.replaceAll("\r\n", " ").replaceAll("\n", " ");
-        }
+        sql = deleteWhitespace(sql);
         log.debug(Log_Sql, sql);
         if (paramMap != null) {
             String paramMapStr = getParamMapStr(paramMap);
@@ -53,9 +51,7 @@ public class SqlLoggerUtils {
         if (!log.isDebugEnabled()) {
             return;
         }
-        if (sql != null && (sql.contains("\r\n") || sql.contains("\n"))) {
-            sql = sql.replaceAll("\r\n", " ").replaceAll("\n", " ");
-        }
+        sql = deleteWhitespace(sql);
         log.debug(Log_Sql, sql);
         if (paramMapList != null) {
             for (Map<String, Object> paramMap : paramMapList) {
@@ -150,6 +146,42 @@ public class SqlLoggerUtils {
                 }
                 sb.append("(").append(valueType).append(")");
             }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 删除多余的空白字符
+     */
+    private static String deleteWhitespace(String sql) {
+        if (sql == null) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder(sql.length());
+        boolean preIsWhitespace = false;
+        for (int i = 0; i < sql.length(); i++) {
+            char ch = sql.charAt(i);
+            boolean isWhitespace = Character.isWhitespace(ch);
+            if (preIsWhitespace) {
+                // 之前是空白字符
+                if (isWhitespace) {
+                    // 当前是空白字符
+                    continue;
+                } else {
+                    // 当前非空白字符
+                    sb.append(ch);
+                }
+            } else {
+                // 之前非空白字符
+                if (isWhitespace) {
+                    // 当前是空白字符
+                    sb.append(' ');
+                } else {
+                    // 当前非空白字符
+                    sb.append(ch);
+                }
+            }
+            preIsWhitespace = isWhitespace;
         }
         return sb.toString();
     }
