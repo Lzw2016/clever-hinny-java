c2802c8c4083fa59ca7e93839c178c433e050183
lizw <1183409807@qq.com>
2020-10-04 23:12:58 +0800

    hinny-freemarker 65%

--- hinny-freemarker/src/main/java/org/clever/hinny/freemarker/FreeMarkerConfigurer.java
+++ hinny-freemarker/src/main/java/org/clever/hinny/freemarker/FreeMarkerConfigurer.java
@@ -2,6 +2,7 @@ package org.clever.hinny.freemarker;
 
 import freemarker.cache.StringTemplateLoader;
 import freemarker.template.Configuration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
  * 创建时间：2020/10/04 22:17 <br/>
  */
 @Component
+@Slf4j
 public class FreeMarkerConfigurer implements ApplicationContextAware {
     /**
      * FreeMarker容器
@@ -28,6 +30,7 @@ public class FreeMarkerConfigurer implements ApplicationContextAware {
         try {
             CONFIG = applicationContext.getBean(Configuration.class);
         } catch (Exception e) {
+            log.warn("ApplicationContext.getBean(Configuration.class)失败: {}", e.getMessage());
             CONFIG = createConfiguration();
         }
     }
