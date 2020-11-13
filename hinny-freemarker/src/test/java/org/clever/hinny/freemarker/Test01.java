17b467506fbab7bc2f8edfa451af24f1faa86ffd
ymx <mlrsyz@vip.qq.com>
2020-11-13 14:21:55 +0800

    模板用例

--- hinny-freemarker/src/test/java/org/clever/hinny/freemarker/Test01.java
+++ hinny-freemarker/src/test/java/org/clever/hinny/freemarker/Test01.java
@@ -1,6 +1,5 @@
 package org.clever.hinny.freemarker;
 
-import freemarker.cache.StringTemplateLoader;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -11,6 +10,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -23,25 +23,20 @@ public class Test01 {
 
     @Test
     public void t01() {
-        try {
-            String result = "";
+        try (Writer writer = new StringWriter()) {
             Configuration cfg = new Configuration(Configuration.VERSION_2_3_30);
             cfg.setNumberFormat("0.##");
             cfg.setClassicCompatible(true);
+            cfg.setEncoding(Locale.CHINA, "utf-8");
+            cfg.setClassForTemplateLoading(this.getClass(), "/templates/");
+            Template template = cfg.getTemplate("msg.ftl", FreeMarkerUtils.Default_Encoding);
             Map<String, Object> paramMap = new HashMap<>();
-            paramMap.put("name", "Brady");
-            paramMap.put("age", "18岁");
-            paramMap.put("high", "180cm");
-            StringTemplateLoader stringLoader = new StringTemplateLoader();
-            String templateContent = "${name} /${age} /${high}";
-            stringLoader.putTemplate("myTemplate", templateContent);
-            cfg.setTemplateLoader(stringLoader);
-            Template template = cfg.getTemplate("myTemplate", FreeMarkerUtils.Default_Encoding);
-            Writer writer = new StringWriter();
+            paramMap.put("url", "entry.getKey()");
+            paramMap.put("MM", "1211112313");
+            paramMap.put("date", "2上次那家超时订单");
+            paramMap.put("subject", "ssl证书过期提醒");
             template.process(paramMap, writer);
-            result = writer.toString();
-            writer.flush();
-            log.info("res=========>{}", result);
+            log.info("res=========>\n{}", writer.toString());
         } catch (IOException | TemplateException e) {
             e.printStackTrace();
         }
---
+++ hinny-freemarker/src/test/resources/templates/msg.ftl
@@ -0,0 +1,15 @@
+<style>
+    .div1 {
+        display: inline-block;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+    }
+</style>
+<body>
+<div class="div1">地址:<a href="${url}">${url}</a></div>
+<div class="div1">还有:${MM}天过期</div>
+<div class="div1">证书到期时间:${date}</div>
+<br/>
+</body>
+
